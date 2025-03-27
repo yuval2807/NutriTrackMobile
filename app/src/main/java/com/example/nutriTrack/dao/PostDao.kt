ed624f0934d7282600eb4061aa4bd9d0503bbd33
Yuval Hikry <hashlama53@bsmch.net>
2025-03-27 16:53:17 +0200

    add rooms

---
+++ app/src/main/java/com/example/nutriTrack/dao/PostDao.kt
@@ -0,0 +1,25 @@
+package  com.example.nutriTrack.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.nutriTrack.Model.Post
+
+@Dao
+interface PostDao {
+
+    @Query("SELECT * FROM Post")
+    fun getAllPost(): LiveData<List<Post>>
+
+    @Query("SELECT * FROM Post WHERE id =:id")
+    fun getPostById(id: String): Post
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg student: Post)
+
+    @Delete
+    fun delete(student: Post)
+}
