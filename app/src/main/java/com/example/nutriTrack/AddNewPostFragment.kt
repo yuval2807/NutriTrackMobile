3b6edf503a223209b3ad24a622c50a11cf32030a
inbar112 <inbaradany@gmail.com>
2025-03-29 16:30:17 +0300

    sync remote and local DB

--- app/src/main/java/com/example/nutriTrack/AddNewPostFragment.kt
+++ app/src/main/java/com/example/nutriTrack/AddNewPostFragment.kt
@@ -4,10 +4,16 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
@@ -153,7 +159,8 @@ class AddNewPostFragment : Fragment() {
             description,
             postImageUri?.toString() ?: "",
             "user1",
-            getCurrDate()
+            getCurrDate(),
+            System.currentTimeMillis()
         )
 
         if (postImageBitmap != null) {
--- app/src/main/java/com/example/nutriTrack/dao/AppLocalDb.kt
+++ app/src/main/java/com/example/nutriTrack/dao/AppLocalDb.kt
@@ -1,5 +1,6 @@
     package  com.example.nutriTrack.dao
 
+    import android.content.Context
     import androidx.room.Database
     import androidx.room.Room
     import androidx.room.RoomDatabase
@@ -13,6 +14,8 @@
 
     object AppLocalDb {
 
+        private const val LOCAL_LAST_UPDATE = "last_update_date"
+
         val database: AppLocalDbRepository by lazy {
 
             val context = MyApplication.Globals.context ?: throw IllegalStateException("Application context is missing")
@@ -24,5 +27,17 @@
             )
                 .fallbackToDestructiveMigration()
                 .build()
+        }
+
+        fun saveLastUpdateDate(timestamp: Long) {
+            val context = MyApplication.Globals.context ?: throw IllegalStateException("Application context is missing")
+            val sharedPreferences = context.getSharedPreferences("TAG", Context.MODE_PRIVATE)
+            sharedPreferences.edit().putLong(LOCAL_LAST_UPDATE, timestamp).apply()
+        }
+
+        fun getLastUpdateDate(): Long {
+            val context = MyApplication.Globals.context ?: throw IllegalStateException("Application context is missing")
+            val sharedPreferences = context.getSharedPreferences("TAG", Context.MODE_PRIVATE)
+            return sharedPreferences.getLong(LOCAL_LAST_UPDATE, 0)
         }
     }
